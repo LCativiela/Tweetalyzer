045240d54df9d0247e8f09928569fac1caad9133
Lorenzo <lorenzocativiela@gmail.com>
2022-08-28 16:52:30 +0200

    Created react ap

---
+++ client/src/components/Input.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+class Input extends Component{
+    constructor(){
+        super();
+    
+    
+    }
+
+    render(){
+
+        const { task, tasks } = this.state;
+
+        return(
+        );
+    }
+
+}
+
+export default Input;
